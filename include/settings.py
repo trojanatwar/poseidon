0831e22575e66874d1d807b119e4b7baf0a82b16
Andrea Pasciuta <sidus@arbornet.org>
2016-09-29 01:14:45 +0200

    Update 0.2.4

--- include/settings.py
+++ include/settings.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Poseidon. If not, see <http://www.gnu.org/licenses/>.
 
-import os, sys, gettext, pickle, sqlite3 as lite, subprocess
+import os, gettext, pickle, sqlite3 as lite, subprocess
 
 '''
 #####################
